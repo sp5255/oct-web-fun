3b37d7dc5df0c58ecc155b4ea36810edc095a558
sp5255 <pundir5255@gmail.com>
2021-12-12 19:11:37 +0530

    included error handling if the api return timeout

--- HandsOn 12_dec Test/index.js
+++ HandsOn 12_dec Test/index.js
@@ -1,26 +1,34 @@
+let memeImg = document.getElementById("meme-img");
+let generateMeme = document.getElementById("genereate-meme");
+memeImg.src = "https://i.redd.it/a4lmlplaxy481.jpg";
 
-let memeImg = document.getElementById('meme-img');
-let generateMeme = document.getElementById('genereate-meme');
-memeImg.src = "https://i.redd.it/a4lmlplaxy481.jpg"
-
-
-generateMeme.addEventListener('click', function(){    
-    generateMeme.innerText = 'Generating Meme';
-    generateMeme.innerHTML += '<i></i>'    
+generateMeme.addEventListener("click", function () {
+    generateMeme.innerText = "Generating Meme";
+    generateMeme.innerHTML += "<i></i>";
     generateMeme.disabled = true;
-    generateMeme.className = 'btn btn-outline-danger w-50 my-2 bg-danger text-white '
+    generateMeme.className =
+        "btn btn-outline-danger w-50 my-2 bg-danger text-white ";
 
+    let spinner = this.children[0];
+    spinner.className = "fa fa-spinner fa-spin mx-3";
 
-    let spinner = this.children[0];    
-    spinner.className = "fa fa-spinner fa-spin mx-3";    
-
-    fetch('https://meme-api.herokuapp.com/gimme')
-    .then(Response => Response.json()).then(data =>{
-        console.log(data)
-        memeImg.src = data.url
-        generateMeme.innerHTML = 'Generate Meme'
-        generateMeme.disabled = false;
-        generateMeme.className = 'btn btn-outline-danger w-50 my-2'
-    });
-
-})
+    fetch("https://meme-api.herokuapp.com/gimme")
+        .then((Response) => {
+            if (Response.ok) return Response.json();
+            else throw new Error("something went wrong");
+        })
+        .then((data) => {
+            console.log(data);
+            memeImg.src = data.url;
+            generateMeme.innerHTML = "Generate Meme";
+            generateMeme.disabled = false;
+            generateMeme.className = "btn btn-outline-danger w-50 my-2";
+        })
+        .catch((error) => {
+            alert("something went wrong ! please try again");
+            console.log(eroot);
+            generateMeme.innerHTML = "Generate Meme";
+            generateMeme.disabled = false;
+            generateMeme.className = "btn btn-outline-danger w-50 my-2";
+        });
+});
